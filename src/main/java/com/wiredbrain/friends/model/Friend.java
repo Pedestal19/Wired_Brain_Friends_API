4c64965188119af01868879fa1d93653837078e3
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-03 08:04:53 +0100

    added more attributes to friends with json annotations

--- src/main/java/com/wiredbrain/friends/model/Friend.java
+++ src/main/java/com/wiredbrain/friends/model/Friend.java
@@ -1,6 +1,9 @@
 package com.wiredbrain.friends.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,8 +21,13 @@ public class Friend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @JsonProperty("first-name")
     private String firstname;
+    @JsonProperty("last-name")
     private String lastName;
+    private int age;
+    @JsonIgnore
+    boolean married;
 
     public int getId() {
         return id;
@@ -43,5 +51,21 @@ public class Friend {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
     }
 }
