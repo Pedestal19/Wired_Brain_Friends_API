49653477855aaaa2bee77a46e89a26f839dfb7de
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-06 02:22:52 +0100

    using spring security http basic

--- src/main/java/com/wiredbrain/friends/FriendsApplication.java
+++ src/main/java/com/wiredbrain/friends/FriendsApplication.java
@@ -2,9 +2,22 @@ package com.wiredbrain.friends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class FriendsApplication {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(User.withDefaultPasswordEncoder()
+                .username("hosanna")
+                .password("hosanna")
+                .roles("USER")
+                .build());
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FriendsApplication.class, args);
